717321fd2946395e89c4f652dd28f7546e2aea1d
tyler shao <tyrelltle@gmail.com>
2013-04-24 23:12:39 -0700

    revert getimageinf servlet

--- allenwebsite/src/com/allen/website/GetImageInfoServlet.java
+++ allenwebsite/src/com/allen/website/GetImageInfoServlet.java
@@ -21,18 +21,28 @@ public class GetImageInfoServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
 		PersistenceManager pm = PMF.get().getPersistenceManager();
-		
-		
-        // Search for any Movie object with the passed-in title; limit the number
-        // of results returned to 1 since there should be at most one movie with
-        // a given title
-        Query query = pm.newQuery(Picture.class);
 
-       
-       List<Picture> results = (List<Picture>) query.execute();
-       
-		
-		pm.close();
+		Query query = pm.newQuery(Picture.class);
+
+		List<Picture> results = (List<Picture>) query.execute();
+		JSONObject json = new JSONObject();
+		try {
+
+			Iterator<Picture> i = results.iterator();
+			while (i.hasNext()) {
+				Picture p = (Picture) i.next();
+				json.put(p.getId().toString(), p.getTitle());// p.getTitle());
+
+			}
+
+			resp.setContentType("application/json");
+			resp.getWriter().write(json.toString());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
 		
 		
 	}
